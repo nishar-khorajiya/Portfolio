2d2edbd02f8a37ba929d4e36b668cddf44a86bab
Nishar Khorajiya <110724795+nishar-khorajiya@users.noreply.github.com>
2025-01-09 11:04:07 +0530

    first commit

---
+++ src/Components/Projects.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { AiTwotoneAppstore } from "react-icons/ai";
+import { MdOutlineFlipCameraIos } from "react-icons/md";
+import { FaCode } from "react-icons/fa";
+import "../Components/Global.css";
+import project1 from "../Components/SocialLinks/e commerce ss.png"
+import project2 from "../Components/SocialLinks/portfolio project ss.png"
+import project3 from "../Components/SocialLinks/text analyzer ss.png"
+import project4 from "../Components/SocialLinks/job .png"
+
+
+const Projects = ({ bgcolor, darkmode }) => {
+  return (
+    <>
+      {/* <div className="p-3"> */}
+      <div className={` ${darkmode === "dark" ? `bg-${bgcolor}` : `bg-white`} p-10 rounded-lg`}>
+        <h1 className={`text-3xl font-bold ${bgcolor === "white" ? "text-black" : "text-white"}`}>Projects</h1>
+
+        <div className="flex gap-5 flex-wrap my-5">
+          <a href="https://github.com/nishar-khorajiya/SGP-3">
+            <div
+              className="ui-ux-design w-80 flex flex-col gap-3 p-5 rounded-lg"
+              style={{ background: `${bgcolor === "white" ? "#FFF4F4" : "black"}`, border: `${bgcolor === "white" ? "none" : "1px solid #A6A6A6"}` }}
+            >
+              <img
+                src={project1}
+                className="rounded-lg transition delay-150"
+              />
+              <div>
+
+                <h1 className={`text-md font-bold ${bgcolor === "white" ? "text-black" : "text-white"}`}>E-commerce Website - Ashutosh Enterprise (HTML ,CSS ,Bootstrap ,JavaScript ,ReactJs ,NodeJS ,ExpressJs ,Mongodb)</h1>
+              </div>
+            </div>
+          </a>
+
+          <a href="https://www.luvkushchildrenhospital.in/#/home">
+            <div
+              className="ui-ux-design w-80 flex flex-col gap-3 p-5 rounded-lg"
+              style={{ background: `${bgcolor === "white" ? "#EEF5FA" : "black"}`, border: `${bgcolor === "white" ? "none" : "1px solid #A6A6A6"}` }}
+            >
+              <img
+                src={project2}
+                className="rounded-lg transition delay-150"
+              />
+              <div>
+
+                <h1 className={`text-md font-bold ${bgcolor === "white" ? "text-black" : "text-white"}`}>LuvKush Children's Hospital Portal (Html, Css, javascript)</h1>
+              </div>
+            </div>
+          </a>
+          <a href="https://hotel-world-zeta.vercel.app/">
+            <div
+              className="ui-ux-design w-80 flex flex-col gap-3 p-5 rounded-lg"
+              style={{ background: `${bgcolor === "white" ? "#FFF4F4" : "black"}`, border: `${bgcolor === "white" ? "none" : "1px solid #A6A6A6"}` }}
+            >
+              <img
+                src={project3}
+                className="rounded-lg transition delay-150"
+              />
+              <div>
+
+                <h1 className={`text-md font-bold ${bgcolor === "white" ? "text-black" : "text-white"}`}>Published NPM Package (JavaScript)</h1>
+              </div>
+            </div>
+          </a>
+          <a href="https://github.com/nishar-khorajiya/Job-Seeking-Web-Application/tree/master">
+            <div
+              className="ui-ux-design w-80 flex flex-col gap-3 p-5 rounded-lg"
+              style={{ background: `${bgcolor === "white" ? "#EEF5FA" : "black"}`, border: `${bgcolor === "white" ? "none" : "1px solid #A6A6A6"}` }}
+            >
+              <img
+                src={project4}
+                className="rounded-lg transition delay-150"
+              />
+              <div>
+
+                <h1 className={`text-md font-bold ${bgcolor === "white" ? "text-black" : "text-white"}`}>JOB Portal (HTML ,CSS ,Bootstrap ,JavaScript ,ReactJs ,NodeJS ,ExpressJs , Sequelize ,MySql.)</h1>
+              </div>
+            </div>
+          </a>
+        </div>
+        
+      </div>
+      {/* </div> */}
+    </>
+  );
+};
+
+export default Projects;
---
+++ src/Components/Resume.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { IoBookOutline } from "react-icons/io5";
+import { AiOutlineFileDone } from "react-icons/ai";
+import "../Components/Global.css";
+
+const Resume = ({bgcolor,darkmode}) => {
+  return (
+    
+      <></>
+  );
+};
+
+export default Resume;
